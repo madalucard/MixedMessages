0e01becafcfdd588663b55f6139e54c3c169fe23
Martin Lapihuska <madalucard@gmail.com>
2021-01-10 16:18:37 +0100

    finished exercise challenge app

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const workout = {
+    workoutTypes: ["HIIT", "AMRAP", "TABATA", "Weight", "Stretching"],
     cardioTypes : ["Run", "Rumping Rope", "Swiming", "Bike"],
-    bodyParts: ["Shoulder", "Back", "Chest", "Arms", "Abs", "Legs"],
-    workoutType: ["HIIT", "AMRAP", "TABATA", "Weight Training", "Stretching"]
+    bodyParts: ["Shoulders", "Back", "Chest", "Arms", "Abs", "Legs"]    
 }
+
+const getMessage = () => {
+    let workoutType = workout.workoutTypes[Math.floor(Math.random()* workout.workoutTypes.length)];
+    let cardioType = workout.cardioTypes[Math.floor(Math.random()* workout.cardioTypes.length)];
+    let bodyPart = workout.bodyParts[Math.floor(Math.random()* workout.bodyParts.length)];
+    
+    let message = `
+    =====================CHALLENGE=====================
+    For your today exercise challange will be ${workoutType} training. 
+    You will start with ${cardioType} as cardio.
+    Your focused body part will be ${bodyPart}.
+    Have fun!`;
+    console.log(message)
+}
+
+getMessage();
